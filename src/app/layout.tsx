76220e145723574d61bce6bf8a12c860b37e7572
fernandoprestes <fernandoprestesc@gmail.com>
2023-05-14 09:54:11 -0300

    chore: update deps

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div>layout</div>
+        {children}
+      </body>
     </html>
   )
 }
